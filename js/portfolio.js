a920bdcab6deeddbf5d72a7ccb45453491a5ba42
patemoo <patemoo@comcast.net>
2015-02-11 18:07:41 -0800

    add listo description

--- js/portfolio.js
+++ js/portfolio.js
@@ -20,12 +20,12 @@ app.controller('MuseumController', ['$scope','$sce', function($scope,$sce){
     {name:'Pantry 2 Plate',
     url: $sce.trustAsResourceUrl('https://pantry2plate.herokuapp.com'),
     github: 'https://github.com/patemoo/pantry_2_plate',
-    desc: 'Pantry 2 Plate is a Node.js app that uses the Yummly api. Users add ingredients to their digital pantry, then search or browse through a curated list of recipes that have been filtered based on pantry items.'},
+    desc: 'Pantry 2 Plate is a Node.js app that uses the Yummly API. Users add ingredients to their digital pantry, then search or browse through a curated list of recipes that have been filtered based on pantry items.'},
 
     {name:'Listo',
     url: $sce.trustAsResourceUrl('https://getlisto-pm.herokuapp.com'),
     github: 'https://github.com/patemoo/listo',
-    desc: '... '}
+    desc: 'Users can use Listo to meet others in their neighborhood. An event is created when 4 users in a given area choose the same activity. Foursquare\'s API is used to pull in a meeting location then users communicate over a web sockets chat interface. For accountability a flake score is associated with each user.'}
 
   ]
 

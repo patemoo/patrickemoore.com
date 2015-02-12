4006565e11680b2fb69938ef562d4ddc37d79409
patemoo <patemoo@comcast.net>
2015-02-12 12:28:43 -0800

    add static site images for mobile

--- js/portfolio.js
+++ js/portfolio.js
@@ -20,11 +20,13 @@ app.controller('MuseumController', ['$scope','$sce', function($scope,$sce){
     {name:'Pantry 2 Plate',
     url: $sce.trustAsResourceUrl('https://pantry2plate.herokuapp.com'),
     github: 'https://github.com/patemoo/pantry_2_plate',
+    image: 'pantry2plate-image.jpg',
     desc: 'Pantry 2 Plate is a Node.js app that uses the Yummly API. Users add ingredients to their digital pantry, then search or browse through a curated list of recipes that have been filtered based on pantry items.'},
 
     {name:'Listo',
     url: $sce.trustAsResourceUrl('https://getlisto-pm.herokuapp.com'),
     github: 'https://github.com/patemoo/listo',
+    image: 'listo-image.jpg',
     desc: 'Users can use Listo to meet others in their neighborhood. An event is created when 4 users in a given area choose the same activity. Foursquare\'s API is used to pull in a meeting location then users communicate over a web sockets chat interface. For accountability a flake score is associated with each user. Listo is a Ruby on Rails app.'}
 
   ]
@@ -35,6 +37,7 @@ app.controller('MuseumController', ['$scope','$sce', function($scope,$sce){
     $scope.github = $scope.sites[i].github;
     $scope.name = $scope.sites[i].name;
     $scope.desc = $scope.sites[i].desc;
+    $scope.image = $scope.sites[i].image;
   };
 
   $scope.switchSize = function(screen) {

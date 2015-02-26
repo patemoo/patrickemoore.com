a40e90aa46c4495856e651aabe8d64caa531f707
patemoo <patemoo@comcast.net>
2015-02-25 16:51:06 -0800

    add Departure to gallery

--- js/portfolio.js
+++ js/portfolio.js
@@ -27,7 +27,13 @@ app.controller('MuseumController', ['$scope','$sce', function($scope,$sce){
     url: $sce.trustAsResourceUrl('https://getlisto-pm.herokuapp.com'),
     github: 'https://github.com/patemoo/listo',
     image: 'listo-image.jpg',
-    desc: 'Users can use Listo to meet others in their neighborhood. An event is created when 4 users in a given area choose the same activity. Foursquare\'s API is used to pull in a meeting location then users communicate over a web sockets chat interface. For accountability a flake score is associated with each user. Listo is a Ruby on Rails app.'}
+    desc: 'Users can use Listo to meet others in their neighborhood. An event is created when 4 users in a given area choose the same activity. Foursquare\'s API is used to pull in a meeting location then users communicate over a web sockets chat interface. For accountability a flake score is associated with each user. Listo is a Ruby on Rails app.'},
+
+    {name:'Departure',
+    url: $sce.trustAsResourceUrl('http://departuredashboard.herokuapp.com/'),
+    github: 'https://github.com/patemoo/departure',
+    image: 'departure-image.jpg',
+    desc: 'Departure was created to be a one stop shop for information regarding a user\'s flight travel plans. Built with Sails, SASS, AngularJS and MongoDB Departure uses multiple APIs including FlightStats, Forecast for Developers, Foursquare and Google Maps to pull in relevant data to assist the user as they embark on their trip.'}
 
   ]
 
